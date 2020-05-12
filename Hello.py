0829b7a0820b66f9a54c768cf79ba787cd27daa8
lexel225 <lexel225@gmail.com>
2020-05-12 15:58:08 +0800

    Fix sonarcloud warning

--- Hello.py
+++ Hello.py
@@ -2,23 +2,17 @@
 class Hello:
     def __init__(self, name):
         self.name = name
-        return
 
-    def sayHello(self, yourname):
+    def say_hello(self, yourname):
         print("Hello, %s. I'm %s" % (yourname, self.name))
-        return
     
     def get_true(self):
         return True
 
 def main():
-    #list_test_run()
-    #tuple_test_run()
-    #dictionary_test_run()
-    #set_test_run()
 
     A = Hello("Lexel")
-    A.sayHello("Kay")
+    A.say_hello("Kay")
 
     print("Welcome back {}".format('Lexel'))
 
