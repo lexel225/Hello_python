7a268254bfae4ef9d11824b6f83fd2011ac1710d
Lexel Yu <Lexel_Yu@asus.com>
2020-12-23 23:48:46 +0800

    Update tree.py

--- tree.py
+++ tree.py
@@ -7,12 +7,74 @@ class TreeNode:
         self.right = right_child
 
 class Tree:
+
     def __init__(self, root):
         self.root = root
 
+    def traverse(self, node):
+        print(node.key)
 
-root = TreeNode(1)
+    def InOrder_traversal(self, root):
+        if root is None:
+            return
+        
+        self.InOrder_traversal(root.left)
+        self.traverse(root)
+        self.InOrder_traversal(root.right)
+
+
+    def PreOrder_traversal(self, root):
+        if root is None:
+            return
+
+        self.traverse(root)
+        self.PreOrder_traversal(root.left)
+        self.PreOrder_traversal(root.right)
+
+
+    def PostOrder_traversal(self, root):
+        if root is None:
+            return
+
+        self.PostOrder_traversal(root.left)
+        self.PostOrder_traversal(root.right)
+        self.traverse(root)
+
+    def LevelOrder_traversal(self, root):
+        
+        node_queue = []
+
+        def enqueue(root):
+            if root is None:
+                return
+            node_queue.append(root)        
+        
+        enqueue(root)
+
+        while len(node_queue) > 0:
+            node = node_queue.pop(0)
+            self.traverse(node)
+            enqueue(node.left)
+            enqueue(node.right)
+
+
+root = TreeNode(1,
+        TreeNode(2, 
+            TreeNode(4), TreeNode(5)),
+        TreeNode(3, 
+            TreeNode(6), TreeNode(7))
+        )
 
 tree = Tree(root)
 
-print(tree.root.key)
+print('InOrder')
+tree.InOrder_traversal(tree.root)
+
+print('PreOrder')
+tree.PreOrder_traversal(tree.root)
+
+print('PostOrder')
+tree.PostOrder_traversal(tree.root)
+
+print('LevelOrder')
+tree.LevelOrder_traversal(tree.root)
