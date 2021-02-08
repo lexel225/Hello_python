80f34910ccac00cd820a824dd01f8b47931581e8
Lexel Yu <Lexel_Yu@asus.com>
2021-02-08 17:13:09 +0800

    Update args and kwargs

--- Hello.py
+++ Hello.py
@@ -11,14 +11,34 @@ class Hello:
     def get_true(self):
         return True
 
+    def print_args(self, a, *args):
+
+        print(f'a: {a}')
+        for idx,i in enumerate(args):
+            print(f'args_{idx}: {i}')
+
+    def print_kwargs(self, **kwargs):
+
+        for key,value in kwargs.items():
+            print(f'{key}: {value}')
+
 def main():
+
+    print("Welcome back {}".format('Lexel'))
 
     list_demo.list_test()
 
     A = Hello("Lexel")
     A.say_hello("Kay")
 
-    print("Welcome back {}".format('Lexel'))
+
+    A.print_args('Head', 'First', 'Second')
+
+    kwargs = {'KEY_1': 1, 'KEY_2': 2}
+
+    A.print_kwargs(**kwargs)
+
+    A.print_kwargs(KEY_3=3, KEY_4=4)
 
     return True
 
